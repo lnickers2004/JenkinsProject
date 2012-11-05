897298b89e8bb48b661badd7fbc7596135c67d4c
Steven McWhorter <smcwhorter@cardinalsolutions.com>
2012-11-05 13:37:36 -0500

    inital project setup

---
+++ gen/R.java.d
@@ -0,0 +1,13 @@
+/Users/smcwhorter/Documents/Android Applications/JenkinsProject/gen/com/cardinal/jenkinsproject/R.java \
+ : /Users/smcwhorter/Documents/Android Applications/JenkinsProject/res/drawable-hdpi/ic_action_search.png \
+/Users/smcwhorter/Documents/Android Applications/JenkinsProject/res/drawable-hdpi/ic_launcher.png \
+/Users/smcwhorter/Documents/Android Applications/JenkinsProject/res/drawable-ldpi/ic_launcher.png \
+/Users/smcwhorter/Documents/Android Applications/JenkinsProject/res/drawable-mdpi/ic_action_search.png \
+/Users/smcwhorter/Documents/Android Applications/JenkinsProject/res/drawable-mdpi/ic_launcher.png \
+/Users/smcwhorter/Documents/Android Applications/JenkinsProject/res/drawable-xhdpi/ic_action_search.png \
+/Users/smcwhorter/Documents/Android Applications/JenkinsProject/res/drawable-xhdpi/ic_launcher.png \
+/Users/smcwhorter/Documents/Android Applications/JenkinsProject/res/layout/activity_main.xml \
+/Users/smcwhorter/Documents/Android Applications/JenkinsProject/res/menu/activity_main.xml \
+/Users/smcwhorter/Documents/Android Applications/JenkinsProject/res/values/strings.xml \
+/Users/smcwhorter/Documents/Android Applications/JenkinsProject/res/values/styles.xml \
+/Users/smcwhorter/Documents/Android Applications/JenkinsProject/bin/AndroidManifest.xml \
---
+++ src/com/cardinal/jenkinsproject/utils/AppHelper.java
@@ -0,0 +1,13 @@
+package com.cardinal.jenkinsproject.utils;
+
+public class AppHelper {
+    
+    public AppHelper(){
+	
+    }
+    
+    public int getValue(){
+	return 1;
+    }
+
+}
