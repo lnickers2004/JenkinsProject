fbfdaeae1482116a57e3db5d2635e0cd562316ef
Steven McWhorter <steven.mcwhorter@gmail.com>
2012-11-05 14:20:02 -0500

    changed onCreate()

--- src/com/cardinal/jenkinsproject/MainActivity.java
+++ src/com/cardinal/jenkinsproject/MainActivity.java
@@ -3,6 +3,7 @@ package com.cardinal.jenkinsproject;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import com.cardinal.jenkinsproject.utils.AppHelper;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +11,9 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        AppHelper thisHelper = new AppHelper();
+        int value = thisHelper.getValue();
     }
 
     @Override
